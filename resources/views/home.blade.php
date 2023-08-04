8f21beb93584c113c207f6e3d97c80b8baadff63
danielmegale <danim1@live.it>
2023-08-04 17:51:14 +0200

    complete task

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,117 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    {{-- Header --}}
-    <section id="header">
-        <header class="container">
-            <nav>
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-                <ul>
-                    <li><a>Characters</a></li>
-                    <li><a>Comics</a></li>
-                    <li><a>Movies</a></li>
-                    <li><a>TV</a></li>
-                    <li><a>Games</a></li>
-                    <li><a>Collectibles</a></li>
-                    <li><a>Videos</a></li>
-                    <li><a>Fans</a></li>
-                    <li><a>News</a></li>
-                    <li><a>Shop</a></li>
-                </ul>
-            </nav>
-        </header>
-    </section>
-    {{-- Jumbotron --}}
-    <section id="jumbotron">
-        <div class="height">
-            <figure class="container">
-                <div class="row" v-for="image in images">
-                    <img :src="getImage(image.image)" alt="">
-                    {{-- <p>{{ image . text }}</p> --}}
-                </div>
-            </figure>
-        </div>
-    </section>
+<?php
+$comics = config('comics');
+?>
+@extends('layout.main_layout')
+@section('main')
     {{-- Main --}}
     <section id="main">
         <main>
             <h1 class="container">Contents goes here </h1>
             <figure class="container">
-                <div v-for="comic in comics">
-                    <div class="card-container">
-                        <img :src="comic.thumb" :key="comic.series" :alt="comic.series">
+                @foreach ($comics as $comic)
+                    <div>
+                        <div class="card-container">
+                            <img src="{{ $comic['thumb'] }}">
+                        </div>
+                        <figcaption>{{ $comic['series'] }}</figcaption>
                     </div>
-                    {{-- <figcaption>{{ comic . series }}</figcaption> --}}
-                </div>
+                @endforeach
             </figure>
         </main>
     </section>
-    {{-- Footer --}}
-    <section id="footer">
-        <div>
-            <main>
-                <div class="container">
-                    <div class="ul-flex">
-                        <div>
-                            <ul>DC COMICS
-                                <li>Characters</li>
-                                <li>Comics</li>
-                                <li>Movies</li>
-                                <li>TV</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                            </ul>
-                            <ul>SHOP
-                                <li>Shop DC</li>
-                                <li>Shop DC Collactibles</li>
-                            </ul>
-                        </div>
-                        <ul>DC
-                            <li>Terms of Use</li>
-                            <li>Privacy plicy(New)</li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshops</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                            <li>Shop Help</li>
-                            <li>Contacts Us</li>
-                        </ul>
-                        <ul>SITES
-                            <li>DC</li>
-                            <li>MAD magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>Dc Power Visa</li>
-                        </ul>
-                    </div>
+    {{-- Jumbotron --}}
+    <section id="jumbotron">
+        <div class="height">
+            <figure class="container">
+                <div class="row">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
                 </div>
-
-            </main>
-            <footer>
-                <div class="container f-flex">
-                    <button>SING-UP NOW</button>
-                    <div class="flex">
-                        <div v-for="icon in icons">
-                            <img :src=" (icon.icon)" alt="">
-                        </div>
-                    </div>
+                <div class="row">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p>DC MERCHANDISE</p>
                 </div>
-            </footer>
+                <div class="row">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div class="row">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+                <div class="row">
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p>DC POWER VISA</p>
+                </div>
+            </figure>
         </div>
     </section>
-
-</body>
-
-</html>
+@endsection
